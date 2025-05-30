0e6096efd15799d106718c0bb7e33238a523cc5b
Isarar Siddique <siddisrar786@gmail.com>
2025-05-31 03:11:12 +0530

    Update MultiStepForm.tsx

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -205,7 +205,7 @@ const MultiStepForm = ({ onComplete }: MultiStepFormProps) => {
       
       allSymptoms.forEach(symptom => formDataToSend.append("symptoms", symptom));
 
-      const response = await fetch("http://34.70.226.50:8000/predict", {
+      const response = await fetch("https://e82a-2409-40e3-39e-4e10-bcd9-e0f6-75d-97a.ngrok-free.app/predict", {
         method: "POST",
         body: formDataToSend
       });
